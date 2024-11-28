dced3ff6205ffae2108295fee3889b528e47bea2
devesh1808 <devesh1808@gmail.com>
2024-11-28 16:49:07 +0530

    added tailwind css for practice

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-2xl">MovieList</div>
+      <div className="text-2xl bg-blue-300 p-4 border-8 border-green-500 rounded-lg m-5">MovieList</div>
     </main>
   );
 };
